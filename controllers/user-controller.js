7640b462577b8514977a6ec5f7482a8b2488e175
Jake Dudum <jakedudum@gmail.com>
2021-05-21 14:45:16 -0700

    Removed account from user-controller, formatted auth.js, and added custom js to profile.html to be able to get cookies which was used to display the username in profile.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,32 +2,32 @@ var db = require("../models");
 
 // Defining methods for the UserController
 module.exports = {
-    find: function(req, res) {
+    find: function (req, res) {
         if (req.isAuthenticated()) {
             db.Blogger
-                .findOne({ where: { uuid: req.session.passport.user }, include: [db.Account]})
-                    .then(function(dbUser){res.json(dbUser)})
-                    .catch(function(err){res.status(422).json(err)});
+                .findOne({ where: { uuid: req.session.passport.user } })
+                .then(function (dbUser) { res.json(dbUser) })
+                .catch(function (err) { res.status(422).json(err) });
         }
         res.status(401).json("error");
     },
-    update: function(req, res) {
+    update: function (req, res) {
         if (req.isAuthenticated()) {
             db.Blogger
                 .update(req.body, { where: { uuid: req.session.passport.user } })
-                .then(function(dbUser){  res.json(dbUser)})
-                .catch(function(err){ res.status(422).json(err)});
+                .then(function (dbUser) { res.json(dbUser) })
+                .catch(function (err) { res.status(422).json(err) });
         }
         res.status(401).json("error");
     },
-    delete: function(req, res) {
+    delete: function (req, res) {
         if (req.isAuthenticated()) {
             db.Blogger
                 .destroy({ where: { uuid: req.session.passport.user } })
-                .then(function(deletedUsed) {
+                .then(function (deletedUsed) {
                     res.json(deletedUsed);
                 })
-                .catch(function(err){ res.status(422).json(err)});
+                .catch(function (err) { res.status(422).json(err) });
         }
         res.status(401).json("error");
     }
